eb083e2f5a676cd48c170f7e1c55fa2a30a7212a
Biplab-Dutta <biplabdutta860@gmail.com>
2021-05-31 20:10:04 +0545

    Minor Refactoring

--- lib/business_logic/bloc/settings_bloc/settings_state.dart
+++ lib/business_logic/bloc/settings_bloc/settings_state.dart
@@ -3,8 +3,10 @@ part of 'settings_bloc.dart';
 class SettingsState extends Equatable {
   final bool appNotification;
   final bool emailNotification;
-  const SettingsState(
-      {@required this.appNotification, @required this.emailNotification});
+  const SettingsState({
+    @required this.appNotification,
+    @required this.emailNotification,
+  });
 
   @override
   List<Object> get props => [appNotification, emailNotification];
